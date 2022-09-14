ae0540540c86a2d6a955f7381d7c1e0944a5c172
gabrieldeori <deori.pessoal@gmail.com>
2022-09-14 11:29:26 -0300

    feat: Rollback and not using listener for hover

--- scripts/catchCards/cardsListeners.js
+++ scripts/catchCards/cardsListeners.js
@@ -6,18 +6,6 @@ function removeClass(class_, element) {
   element.classList.remove(class_);
 }
 
-export function hoverCardsListener() {
-  const CARD_CLASS = "catch-card";
-  const CARD_CLICKED_CLASS = "hovered-catch-card";
-  const cards = Array
-    .from(document.getElementsByClassName(CARD_CLASS));
-
-  cards.forEach((card) => {
-    card.addEventListener("mouseenter", () => {
-      addClass(CARD_CLICKED_CLASS, card);
-    });
-    card.addEventListener("mouseleave", () => {
-      removeClass(CARD_CLICKED_CLASS, card);
-    });
-  });
+export {
+  hoverCardsListener
 }
