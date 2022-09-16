3efca2093202278e47c23cfc0273a07a655553a5
gabrieldeori <deori.pessoal@gmail.com>
2022-09-15 21:20:58 -0300

    feat: Appearing white blur in front of cards

--- scripts/catchCards/cardsListeners.js
+++ scripts/catchCards/cardsListeners.js
@@ -12,12 +12,20 @@ function clickCardListener() {
   const toFind = "catch-card";
   const toAdd = "card-catched";
   const toRemove = "card-canhover";
-  const cards = Array.from(document.getElementsByClassName(toFind));
-  
+  const coverBlurHide = "cover-blur-hidden";
+  const coverBlurShow = "cover-blur-show";
+  const cards = Array
+    .from(document.getElementsByClassName(toFind));
+
   cards.forEach(element => {
     element.addEventListener("click", () => {
       removeClass(toRemove, element);
       addClass(toAdd, element);
+
+      const coverElement = Array
+        .from(document.getElementsByClassName(coverBlurHide))[0];
+      removeClass(coverBlurHide, coverElement);
+      addClass(coverBlurShow, coverElement);
     });
   });
   return { removed: toRemove, added: toAdd }
