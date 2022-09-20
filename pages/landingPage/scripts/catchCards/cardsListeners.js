70e2cf85ed204ba82c9ca447020e8265d56b0774
gabrieldeori <deori.pessoal@gmail.com>
2022-09-20 16:27:28 -0300

    feat: Refactoring card structure

--- pages/landingPage/scripts/catchCards/cardsListeners.js
+++ pages/landingPage/scripts/catchCards/cardsListeners.js
@@ -1,44 +1,11 @@
-function addClass(class_, element) {
-  element.classList.add(class_);
-  return class_;
-}
-
-function removeClass(class_, element) {
-  element.classList.remove(class_);
-  return class_;
-}
-
 function clickCardListener() {
-  const toFind = "catch-card";
-  const toAdd = "card-catched";
-  const toRemove = "card-canhover";
-  const coverBlurHide = "cover-blur-hidden";
-  const coverBlurShow = "cover-blur-show";
-  const cards = Array
-    .from(document.getElementsByClassName(toFind));
-
-  cards.forEach(element => {
-    element.addEventListener("click", () => {
-      const hologram = Array.from(document
-        .getElementsByClassName("hologram_top"))[0];
-
-      removeClass("hologram-active", hologram);
-      addClass("hologram-active", hologram);
-
-      removeClass(toRemove, element);
-      addClass(toAdd, element);
-
-      const coverElement = Array
-        .from(document.getElementsByClassName(coverBlurHide))[0];
-      removeClass(coverBlurHide, coverElement);
-      addClass(coverBlurShow, coverElement);
-    });
+  const cards = Array.from(document.getElementsByClassName("c-holocard"));
+  console.log(cards)
+  cards.forEach(card => {
+    console.log(card);
   });
-  return { removed: toRemove, added: toAdd }
 }
 
 export {
-  addClass,
-  removeClass,
   clickCardListener,
 }
