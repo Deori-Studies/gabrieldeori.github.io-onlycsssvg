937da855bd90edcd9f5c83e25e1f0a6220915534
gabrieldeori <deori.pessoal@gmail.com>
2022-09-20 16:35:45 -0300

    feat: Improving cardsListeners.js function

--- pages/landingPage/scripts/catchCards/cardsListeners.js
+++ pages/landingPage/scripts/catchCards/cardsListeners.js
@@ -1,8 +1,17 @@
 function clickCardListener() {
-  const cards = Array.from(document.getElementsByClassName("c-holocard"));
-  console.log(cards)
+  const CLASS_HOLOCARD = "c-holocard";
+  const CLASS_CLICKED_CARD = "m-clicked__card";
+  const CLASS_CAN_HOVER = "m-can__hover";
+
+  const cards = Array.from(
+    document.getElementsByClassName(CLASS_HOLOCARD)
+  );
+
   cards.forEach(card => {
-    console.log(card);
+    card.addEventListener("click", (_e) => {
+      card.classList.add(CLASS_CLICKED_CARD);
+      card.classList.remove(CLASS_CAN_HOVER);
+    });
   });
 }
 
