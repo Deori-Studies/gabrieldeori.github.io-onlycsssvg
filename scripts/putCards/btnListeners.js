9e7ce18a92572b14edbc70e74f77c607e93938bc
gabrieldeori <deori.pessoal@gmail.com>
2022-09-22 13:27:34 -0300

    fix: holocontent start to repeat, add empty inner html on holocontent

--- scripts/putCards/btnListeners.js
+++ scripts/putCards/btnListeners.js
@@ -4,6 +4,7 @@ import IDS from "../variables/IDS.js";
 function btnPutCardListener() {
 
   const btnPutCard = document.getElementById(IDS.BTN_PUTCARD);
+  const holocontent = document.getElementById(IDS.HOLOCONTENT);
   const hologram = document.getElementsByClassName(CLASSES.HOLOGRAM)[0];
   const holocover = document.getElementsByClassName(CLASSES.HOLOCOVER)[0];
   const cards = Array
@@ -16,13 +17,15 @@ function btnPutCardListener() {
     actualCard.classList.remove(CLASSES.CLICKED_CARD);
     actualCard.classList.add(CLASSES.TAKENOUT);
 
-    hologram.classList.remove(CLASSES.ACTIVE)
-    holocover.classList.remove(CLASSES.ACTIVE)
+    hologram.classList.remove(CLASSES.ACTIVE);
+    holocover.classList.remove(CLASSES.ACTIVE);
 
     cards.forEach((card) => {
       card.classList.add(CLASSES.CLICKABLE);
       card.classList.add(CLASSES.CAN_HOVER);
-    })
+    });
+
+    holocontent.innerHTML = "";
   });
 }
 
