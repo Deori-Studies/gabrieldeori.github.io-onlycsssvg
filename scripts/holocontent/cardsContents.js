57aaaab5ff6fc62f7816b4e5376a604ae2448a59
gabrieldeori <deori.pessoal@gmail.com>
2022-09-22 14:37:16 -0300

    feat: removed model commented in cards contents

--- scripts/holocontent/cardsContents.js
+++ scripts/holocontent/cardsContents.js
@@ -1,39 +1,3 @@
-/*
-Model
-
-<header>
-  <h1>CRIATIVIDADE COMO VALOR</h1>
-</header>
-<main>
-  <p>
-  A criatividade é uma competência de valor e
-  aplicabilidade universais, descrita como a
-  capacidade de um indivíduo imaginar, criar,
-  produzir ou inventar conceitos e coisas inéditas.
-  Pode ser entendida também como a ponte entre o
-  pensamento abstrato e o seu correspondente tangível
-  </p><br/>
-
-  <p>
-  A criatividade é uma competência de valor e
-  aplicabilidade universais, descrita como a
-  capacidade de um indivíduo imaginar, criar,
-  produzir ou inventar conceitos e coisas inéditas.
-  Pode ser entendida também como a ponte entre o
-  pensamento abstrato e o seu correspondente tangível
-  </p><br/>
-
-  <p>
-  A criatividade é uma competência de valor e
-  aplicabilidade universais, descrita como a
-  capacidade de um indivíduo imaginar, criar,
-  produzir ou inventar conceitos e coisas inéditas.
-  Pode ser entendida também como a ponte entre o
-  pensamento abstrato e o seu correspondente tangível
-  </p><br/>
-</main>
-*/
-
 import IDS from "../variables/IDS.js";
 
 const cardsContents = {
