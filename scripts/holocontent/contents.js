c240e6bcd457781a046146a01114b8496450a992
gabrieldeori <deori.pessoal@gmail.com>
2022-09-22 11:22:47 -0300

    feat: Differents contents for each card, not concluded

---
+++ scripts/holocontent/contents.js
@@ -0,0 +1,83 @@
+/*
+Model
+
+<header>
+  <h1>CRIATIVIDADE COMO VALOR</h1>
+</header>
+<main>
+  <p>
+  A criatividade é uma competência de valor e
+  aplicabilidade universais, descrita como a
+  capacidade de um indivíduo imaginar, criar,
+  produzir ou inventar conceitos e coisas inéditas.
+  Pode ser entendida também como a ponte entre o
+  pensamento abstrato e o seu correspondente tangível
+  </p><br/>
+
+  <p>
+  A criatividade é uma competência de valor e
+  aplicabilidade universais, descrita como a
+  capacidade de um indivíduo imaginar, criar,
+  produzir ou inventar conceitos e coisas inéditas.
+  Pode ser entendida também como a ponte entre o
+  pensamento abstrato e o seu correspondente tangível
+  </p><br/>
+
+  <p>
+  A criatividade é uma competência de valor e
+  aplicabilidade universais, descrita como a
+  capacidade de um indivíduo imaginar, criar,
+  produzir ou inventar conceitos e coisas inéditas.
+  Pode ser entendida também como a ponte entre o
+  pensamento abstrato e o seu correspondente tangível
+  </p><br/>
+</main>
+*/
+
+import IDS from "../variables/IDS";
+
+const contents = {
+  [IDS.HOLOCARD1]: [
+    {
+      h1: "CRIATIVIDADE"
+    },
+    {
+      p: "A criatividade é uma competência de valor e \
+      aplicabilidade universais, descrita como a \
+      capacidade de um indivíduo imaginar, criar, \
+      produzir ou inventar conceitos e coisas inéditas. \
+      Pode ser entendida também como a ponte entre o \
+      pensamento abstrato e o seu correspondente tangível"
+    },
+  ],
+  [IDS.HOLOCARD2]: [
+    {
+      h1: "TEAMWORK"
+    },
+    {
+      p: "A criatividade é uma competência de valor e \
+      aplicabilidade universais, descrita como a \
+      capacidade de um indivíduo imaginar, criar, \
+      produzir ou inventar conceitos e coisas inéditas. \
+      Pode ser entendida também como a ponte entre o \
+      pensamento abstrato e o seu correspondente tangível"
+    },
+  ],
+  [IDS.HOLOCARD3]: [
+    {
+      h1: "BOAS-PRÁTICAS"
+    },
+    {
+      p: "A criatividade é uma competência de valor e \
+      aplicabilidade universais, descrita como a \
+      capacidade de um indivíduo imaginar, criar, \
+      produzir ou inventar conceitos e coisas inéditas. \
+      Pode ser entendida também como a ponte entre o \
+      pensamento abstrato e o seu correspondente tangível"
+    },
+  ],
+}
+
+export {
+  contents,
+}
