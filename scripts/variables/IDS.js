e6408a435edc45f9b52d2680635268c14379b8b3
gabrieldeori <deori.pessoal@gmail.com>
2022-09-22 10:54:37 -0300

    feat: add on IDS holocontent as ID

--- scripts/variables/IDS.js
+++ scripts/variables/IDS.js
@@ -1,11 +1,13 @@
-const HOLOCARD1 = "holocard1"
-const HOLOCARD2 = "holocard2"
-const HOLOCARD3 = "holocard3"
-const BTN_PUTCARD = "btnPutCard"
+const BTN_PUTCARD = "btnPutCard";
+const HOLOCARD1 = "holocard1";
+const HOLOCARD2 = "holocard2";
+const HOLOCARD3 = "holocard3";
+const HOLOCONTENT = "holocontent";
 
 export default {
+  BTN_PUTCARD,
   HOLOCARD1,
   HOLOCARD2,
   HOLOCARD3,
-  BTN_PUTCARD,
+  HOLOCONTENT,
 }
