6e4c28439a662b2328412beade7b6ee84517e67b
gabrieldeori <deori.pessoal@gmail.com>
2022-09-22 14:32:29 -0300

    feat: Card now are activated after unique three clicks

--- scripts/catchCards/cardsListeners.js
+++ scripts/catchCards/cardsListeners.js
@@ -1,27 +1,51 @@
 import CLASSES from "../variables/CLASSES.js";
+import IDS from "../variables/IDS.js";
+
+const holocardsInfos = {
+  catchedCards: {},
+}
+
+function mainClassHandler(paramCard, cards) {
+  const hologram = Array
+    .from(document.getElementsByClassName(CLASSES.HOLOGRAM))[0];
+  const holocover = Array
+    .from(document.getElementsByClassName(CLASSES.HOLOCOVER))[0];
+
+  paramCard.classList.add(CLASSES.CLICKED_CARD);
+  hologram.classList.add(CLASSES.ACTIVE);
+  holocover.classList.add(CLASSES.ACTIVE);
+
+  cards.forEach((otherCards) => {
+    otherCards.classList.remove(CLASSES.CLICKABLE);
+    otherCards.classList.remove(CLASSES.CAN_HOVER);
+  });
+}
+
+function countCard(card) {
+  if(!holocardsInfos.catchedCards.hasOwnProperty(card.id)) {
+    holocardsInfos.catchedCards[card.id] = true;
+  }
+
+  if (Object.keys(holocardsInfos.catchedCards).length >= 3) {
+    const extraCard = document.getElementById(IDS.HOLOCARD4);
+    extraCard.classList.remove(CLASSES.DISABLED);
+    extraCard.classList.add(CLASSES.CLICKABLE);
+    extraCard.classList.add(CLASSES.CAN_HOVER);
+  }
+}
 
 function clickCardListener() {
 
   const cards = Array
     .from(document.getElementsByClassName(CLASSES.HOLOCARD));
-  const hologram = Array
-    .from(document.getElementsByClassName(CLASSES.HOLOGRAM))[0];
-  const holocover = Array
-    .from(document.getElementsByClassName(CLASSES.HOLOCOVER))[0]
 
   cards.forEach(card => {
     card.addEventListener("click", (_e) => {
       const clickable = card.classList.contains(CLASSES.CLICKABLE);
-  
-      if (clickable) {
-        card.classList.add(CLASSES.CLICKED_CARD);
-        hologram.classList.add(CLASSES.ACTIVE);
-        holocover.classList.add(CLASSES.ACTIVE);
 
-        cards.forEach((newCard) => {
-          newCard.classList.remove(CLASSES.CLICKABLE);
-          newCard.classList.remove(CLASSES.CAN_HOVER);
-        });
+      if (clickable) {
+        mainClassHandler(card, cards);
+        countCard(card);
       }
     });
   });
